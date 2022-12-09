b2ec0b6c430ab9dccd07f709b5e3a05ef5a808fa
haidao01 <110355637+haidao01@users.noreply.github.com>
2022-12-08 23:50:19 -0400

    Add files via upload

---
+++ lpc-turtle/Fractal_tree.py
@@ -0,0 +1,34 @@
+import turtle
+
+turtle.title("Fractal tree")
+turtle.bgcolor("#B6F5FA")
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+my_turtle.pensize(2)
+my_turtle.color("brown")
+my_turtle.left(90)
+my_turtle.bk(100)
+my_turtle.speed(200)
+
+def fractal_tree(n):
+    if n < 10:
+        return
+    else:
+        my_turtle.fd(n)
+        my_turtle.color("green")
+        my_turtle.circle(2)
+        my_turtle.color("brown")
+        my_turtle.left(30)
+        fractal_tree(3*n/4)
+        my_turtle.right(60)
+        fractal_tree(3*n/4)
+        my_turtle.left(30)
+        my_turtle.bk(n)
+
+fractal_tree(100)
+turtle.done()
+
+
+
+
+
